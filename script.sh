c669c60bebc7d9e4e2182d0cb9344c895906727f
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2021-12-21 15:14:53 +0900

    Echo the debug message (#5)

--- script.sh
+++ script.sh
@@ -18,6 +18,7 @@ platform="${TERRASCAN_PLATFORM:?}"
 curl -L "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/"${version}" | grep -o -E "https://.+?_${platform}.tar.gz")" >terrascan.tar.gz
 tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
 install terrascan /usr/local/bin && rm terrascan
+echo "terrascan is installed at $(which terrascan)."
 echo '::endgroup::'
 
 # Scan
@@ -25,11 +26,11 @@ echo '::group::Scan ...'
 # Allow failures now, as reviewdog handles them
 set +Eeuo pipefail
 
-scan_results="terrascan.results.json"
+scan_results="terrascan-results.json"
 # shellcheck disable=SC2046
 terrascan scan \
   --output json \
-  $(if [[ "x${TERRASCAN_CONFIG_PATH}" != "x" ]]; then echo "--config-PATH ${TERRASCAN_CONFIG_PATH}"; fi) \
+  $(if [[ "x${TERRASCAN_CONFIG_PATH}" != "x" ]]; then echo "--config-path ${TERRASCAN_CONFIG_PATH}"; fi) \
   $(if [[ "x${TERRASCAN_LOG_LEVEL}" != "x" ]]; then echo "--log-level ${TERRASCAN_LOG_LEVEL}"; fi) \
   $(if [[ "x${TERRASCAN_IAC_DIR}" != "x" ]]; then echo "--iac-dir ${TERRASCAN_IAC_DIR}"; fi) \
   $(if [[ "x${TERRASCAN_IAC_TYPE}" != "x" ]]; then echo "--iac-type ${TERRASCAN_IAC_TYPE}"; fi) \
@@ -40,10 +41,10 @@ terrascan scan \
   $(if [[ "x${TERRASCAN_SEVERITY}" != "x" ]]; then echo "--severity ${TERRASCAN_SEVERITY}"; fi) \
   $(if [[ "x${TERRASCAN_SKIP_RULES}" != "x" ]]; then echo "--skip-rules ${TERRASCAN_SKIP-RULES}"; fi) \
   $(if [[ "x${TERRASCAN_USE_COLORS}" != "x" ]]; then echo "--use-colors ${TERRASCAN_USE_COLORS}"; fi) \
-  $(if [[ "x${TERRASCAN_VERBOSE}" != "x" ]]; then echo "--verbose"; fi) \
-  >"$scan_results"
-
+  $(if [[ "x${TERRASCAN_VERBOSE}" != "x" ]]; then echo "--verbose"; fi) |
+  tee "$scan_results"
 terrascan_exit_code=$?
+
 echo "::set-output name=terrascan-results::$(cat <"$scan_results" | jq -r -c '.')" # Convert to a single line
 echo "::set-output name=terrascan-exit-code::${terrascan_exit_code}"
 
