8fe4c19ee1e79b8c4cf5236d3edc26da95bb282c
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2021-12-21 22:32:18 +0900

    test

--- testdata/terraform/google_strage_bucket.tf
+++ testdata/terraform/google_strage_bucket.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "failed_bucket" {
   name = "failed-bucket"
 }
 
-resource "google_storage_bucket" "failed_bucket" {
+resource "google_storage_bucket" "failed_bucket_02" {
   name = "failed-bucket-02"
 }
