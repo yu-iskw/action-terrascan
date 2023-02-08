4844cd444f27a208d6a266b186b8685176c47634
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-02-08 10:49:33 +0900

    Update the manner to set outputs of GitHub Actions (#18)

--- testdata/terraform/google_strage_bucket.tf
+++ testdata/terraform/google_strage_bucket.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "failed_bucket" {
   name = "failed-bucket"
 }
 
+
 resource "google_project_iam_member" "failed_iam" {
   role   = "roles/owner"
   member = "user:test@test.com"
